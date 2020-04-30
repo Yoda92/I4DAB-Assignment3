d0ebbf54fd266769c3fefc274d6cf83da8ae46a3
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-04-30 13:44:54 +0200

    No longer possible to sub to same circle multiple times, and made signout

--- Data/Services/UserService.cs
+++ Data/Services/UserService.cs
@@ -6,6 +6,7 @@ using MongoDB.Driver;
 namespace Data.Services {
     public class UserService : Service<User> {
         public User AddCircleToUser (User user, Circle circle) {
+            if (user.SubscribedCircleIds.Contains (circle.Id)) return user; //If already subbed, do nothing
             user.SubscribedCircleIds.Add (circle.Id);
             var filter = Builders<User>.Filter.Eq ("Id", user.Id);
             var update = Builders<User>.Update.Set ("SubscribedCircleIds", user.SubscribedCircleIds);
@@ -13,10 +14,9 @@ namespace Data.Services {
 
             return user;
         }
-        public List<User> Find(string name)
-        {
-            var users = _collection.Find(user => user.UserName == name);
-            return users.ToList();
+        public List<User> Find (string name) {
+            var users = _collection.Find (user => user.UserName == name);
+            return users.ToList ();
         }
     }
 }
--- Mvc/Controllers/AccountController.cs
+++ Mvc/Controllers/AccountController.cs
@@ -7,55 +7,50 @@ using Data.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Mvc.Controllers
-{
-    public class AccountController : Controller
-    {
+namespace Mvc.Controllers {
+    public class AccountController : Controller {
         private UserService _userService;
 
-        public AccountController()
-        {
-            _userService = new UserService();
+        public AccountController () {
+            _userService = new UserService ();
         }
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index () {
+            return View ();
         }
 
-        public IActionResult SignInUser(string name)
-        {
-            var user = _userService.Find(name);
-            if(user.Count > 0)
-            {
+        public IActionResult SignInUser (string name) {
+            var user = _userService.Find (name);
+            if (user.Count > 0) {
                 Program.CurrentUser = user[0].Id;
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction ("Index", "Home");
         }
 
-        public IActionResult RegisterUser(string name)
-        {
-            var user = _userService.Create(new User()
-            {
+        public IActionResult RegisterUser (string name) {
+            var user = _userService.Create (new User () {
                 UserName = name,
-                Age = 0,
-                BlackListUserIds = new List<string>(),
-                FollowUserIds = new List<string>(),
-                Gender = "Gender Fluid",
-                PostIds = new List<string>(),
-                SubscribedCircleIds = new List<string>()
+                    Age = 0,
+                    BlackListUserIds = new List<string> (),
+                    FollowUserIds = new List<string> (),
+                    Gender = "Gender Fluid",
+                    PostIds = new List<string> (),
+                    SubscribedCircleIds = new List<string> ()
             });
             Program.CurrentUser = user.Id;
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction ("Index", "Home");
         }
 
-        public IActionResult SignIn()
-        {
-            return View();
+        public IActionResult SignOut () {
+            Program.CurrentUser = null;
+            return RedirectToAction ("Index", "Home");
         }
 
-        public IActionResult Register()
-        {
-            return View();
+        public IActionResult SignIn () {
+            return View ();
+        }
+
+        public IActionResult Register () {
+            return View ();
         }
     }
 }
