a93c949620f79e632b1c05df25bb90190e5d13ce
Kristian Lau Jespersen <klj.jep@gmail.com>
2020-04-30 11:21:23 +0200

    Post added

---
+++ Data/Entities/Post.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data
+{
+    public class Post : Entity, IValidatableObject
+    {
+        public string UserId { get; set; }
+        public string CircleId { get; set; }
+        public string ContentType { get; set; }
+        public string Text { get; set; }
+        public string ImagePath { get; set; }
+        public List<Comment> Comments { get; set; }
+        
+        
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ContentType == "Text" && ImagePath!=null)
+            {
+                yield return new ValidationResult("A text post may not contain an image.",new List<string>({"ImagePath"});
+            }
+        }
+    }
+}
