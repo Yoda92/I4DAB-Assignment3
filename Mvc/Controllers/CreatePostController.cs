cc18ee4fc2e4e0c1df18f3db27f37bf148828b8a
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-04-30 15:39:37 +0200

    Pictures and posts

--- Mvc/Controllers/CreatePostController.cs
+++ Mvc/Controllers/CreatePostController.cs
@@ -7,51 +7,46 @@ using Data.Services;
 using Microsoft.AspNetCore.Mvc;
 using Requests;
 
-namespace Mvc.Controllers
-{
-    public class CreatePostController : Controller
-    {
+namespace Mvc.Controllers {
+    public class CreatePostController : Controller {
         private PostService _postService;
-        
-        public CreatePostController()
-        {
-            _postService = new PostService();
+        private UserService _userService;
+
+        public CreatePostController () {
+            _postService = new PostService ();
+            _userService = new UserService ();
         }
-        
-        public async Task<IActionResult> CreatePost(CreatePostRequest request)
-        {
-            var newPost = new Post()
-            {
+
+        public async Task<IActionResult> CreatePost (CreatePostRequest request) {
+            var newPost = new Post () {
                 UserId = Program.CurrentUser,
+                UserName = Program.CurrentUser,
                 CircleId = request.CircleId,
-                Comments = null,
+                CircleName = "Still missing lol",
+                Comments = new List<Comment> (),
                 ContentType = request.ContentType,
                 Text = request.Text,
                 ImagePath = request.ImagePath
             };
 
-            var result = _postService.Create(newPost);
-            return RedirectToAction(request.RequestingView,request.ControllerOfRequestingView);
+            var result = _postService.Create (newPost);
+            _userService.AddPostToUser (_userService.GetById (Program.CurrentUser), result);
+            return RedirectToAction (request.RequestingView, request.ControllerOfRequestingView);
         }
 
-        public ActionResult Index()
-        {
-            return View();
+        public ActionResult Index () {
+            return View ();
         }
-        
-        public ActionResult Index1()
-        {
-            return View();
+
+        public ActionResult Index1 () {
+            return View ();
         }
-        
-        
+
     }
 }
 
-namespace Requests
-{
-    public class CreatePostRequest
-    {
+namespace Requests {
+    public class CreatePostRequest {
         public string UserId { get; set; }
         public string CircleId { get; set; }
         public string ContentType { get; set; }
@@ -59,15 +54,11 @@ namespace Requests
         public string ImagePath { get; set; }
         public string RequestingView { get; set; }
         public string ControllerOfRequestingView { get; set; }
-        
 
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (ContentType == "Text" && ImagePath != null)
-            {
-                yield return new ValidationResult("A text post may not contain an image.",
-                    new List<string>() {"ImagePath"});
+        public IEnumerable<ValidationResult> Validate (ValidationContext validationContext) {
+            if (ContentType == "Text" && ImagePath != null) {
+                yield return new ValidationResult ("A text post may not contain an image.",
+                    new List<string> () { "ImagePath" });
             }
         }
     }
