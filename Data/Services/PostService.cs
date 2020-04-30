9f9cec912c251989f437bf09c5e1592d24cd7783
Yoda92 <andersfisker1007@outlook.com>
2020-04-30 15:45:21 +0200

    Added PostService GetByCircleId

--- Data/Services/PostService.cs
+++ Data/Services/PostService.cs
@@ -1,10 +1,16 @@
+using System.Collections.Generic;
 using Data.Entities;
 using Data.Services;
+using MongoDB.Driver;
 
 namespace Data.Services
 {
     public class PostService : Service<Post>
     {
-        
+        public List<Post> GetByCircleId(string circleId)
+        {
+            var posts = _collection.Find(p => p.CircleId == circleId);
+            return posts.ToList();
+        }
     }
 }
