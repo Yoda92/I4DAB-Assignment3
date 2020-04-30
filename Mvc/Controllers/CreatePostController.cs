fe77f856ebf1011dc8eb00047c8cfb216f8e27ed
Kristian Lau Jespersen <klj.jep@gmail.com>
2020-04-30 14:33:20 +0200

    posts can be created

---
+++ Mvc/Controllers/CreatePostController.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Data;
+using Data.Entities;
+using Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Requests;
+
+namespace Mvc.Controllers
+{
+    public class CreatePostController : Controller
+    {
+        private PostService _postService;
+        
+        public CreatePostController()
+        {
+            _postService = new PostService();
+        }
+        
+        public async Task<IActionResult> CreatePost(CreatePostRequest request)
+        {
+            var newPost = new Post()
+            {
+                UserId = request.UserId,
+                CircleId = request.CircleId,
+                Comments = null,
+                ContentType = request.ContentType,
+                Text = request.Text,
+                ImagePath = request.ImagePath
+            };
+
+            var result = _postService.Create(newPost);
+            return RedirectToAction(request.RequestingView,request.ControllerOfRequestingView);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        
+        
+    }
+}
+
+namespace Requests
+{
+    public class CreatePostRequest
+    {
+        public string UserId { get; set; }
+        public string CircleId { get; set; }
+        public string ContentType { get; set; }
+        public string Text { get; set; }
+        public string ImagePath { get; set; }
+        public string RequestingView { get; set; }
+        public string ControllerOfRequestingView { get; set; }
+        
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ContentType == "Text" && ImagePath != null)
+            {
+                yield return new ValidationResult("A text post may not contain an image.",
+                    new List<string>() {"ImagePath"});
+            }
+        }
+    }
+}
