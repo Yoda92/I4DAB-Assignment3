bf4d5de4b252a7c2dff48f1c92d4d6a639feb890
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-04-30 14:44:50 +0200

    Names in posts

--- Data/Entities/Post.cs
+++ Data/Entities/Post.cs
@@ -4,7 +4,9 @@ using System.ComponentModel.DataAnnotations;
 namespace Data.Entities {
     public class Post : Entity, IValidatableObject {
         public string UserId { get; set; }
+        public string UserName { get; set; }
         public string CircleId { get; set; }
+        public string CircleName { get; set; }
         public string ContentType { get; set; }
         public string Text { get; set; }
         public string ImagePath { get; set; }
