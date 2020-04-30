1f97a02c557b7a0bc3e921ed531945d3111170e9
Yoda92 <andersfisker1007@outlook.com>
2020-04-30 14:56:17 +0200

    Added WallRepository

---
+++ Data/Repositories/WallRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Entities;
+using Data.Services;
+using MongoDB.Driver;
+
+namespace Data.Repositories
+{
+    class WallRepository
+    {
+        private UserService _userService;
+        private PostService _postService;
+        public WallRepository()
+        {
+            _userService = new UserService();
+        }
+        public List<Post> GetPosts(string wallUserId, string queryUserId)
+        {
+            List<Post> wallPosts = new List<Post>();
+            User wallUser = _userService.GetById(wallUserId);
+            User queryUser = _userService.GetById(queryUserId);
+            Post wallUserPost;
+            foreach(string postId in wallUser.PostIds)
+            {
+                wallUserPost = _postService.GetById(postId);
+                if(wallUserPost.circleId == 0 || queryUser.SubscribedCircleIds.Contains(wallUserPost.circleId)) wallPosts.Add(wallUserPost);
+            }
+
+            return wallPosts;
+        }
+    }
+}
